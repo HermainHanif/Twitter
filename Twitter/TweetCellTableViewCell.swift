4f1bdea599a756930beab381ab5ff72cdb359f29
Hamzah <hermainhanif@hermains-macbook-air.local>
2019-02-04 00:11:16 -0800

    removed comment back to original

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -16,7 +16,6 @@ class TweetCellTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
